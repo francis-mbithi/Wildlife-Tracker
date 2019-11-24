5f00cfb49ba8f50a5f3669d5c613f729524b3476
francis-mbithi <frankmbithifm@gmail.com>
2019-11-24 13:49:54 +0300

    fetch sighting from DB

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -37,5 +37,44 @@ public class Sightings {
     public String getFormattedDate() {
         return DateFormat.getDateTimeInstance().format(spotted);
     }
+    @Override
+    public boolean equals(Object anotherSighting){
+        if(!(anotherSighting instanceof Sightings)){
+            return false;
+        }
+        else{
+            Sightings newSighting = (Sightings) anotherSighting;
+            return this.getId() == newSighting.getId() &&
+                    this.getLocation().equals(newSighting.getLocation()) &&
+                    this.getRangerName().equals(newSighting.getRangerName());
+        }
+    }
+
+    public static List<Sightings> all(){
+        String sql = "SELECT * FROM sightings;";
+        try(Connection connect= DB.sql2o.open()){
+            return connect.createQuery(sql).executeAndFetch(Sightings.class);
+        }
+    }
+    public void save(){
+        try(Connection connect= DB.sql2o.open()){
+            String sql ="INSERT INTO sightings (location, rangerName, spotted, animalId) VALUES (:location, :rangerName, now(), :animalId)";
+            this.id = (int) connect.createQuery(sql, true)
+                    .addParameter("location", this.location)
+                    .addParameter("rangerName", this.rangerName)
+                    .addParameter("animalId", this.animalId)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static Sightings find(int id){
+        try(Connection connect= DB.sql2o.open()){
+            String sql ="SELECT * FROM sightings WHERE id=:id;";
+            Sightings sighting = connect.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sightings.class);
+            return sighting;
+        }
+    }
 
 }
