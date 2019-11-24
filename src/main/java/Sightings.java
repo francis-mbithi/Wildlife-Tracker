c944319a7febce02bf221c5f19dede9aa1abe161
francis-mbithi <frankmbithifm@gmail.com>
2019-11-24 13:12:28 +0300

    add sightings location and id

---
+++ src/main/java/Sightings.java
@@ -0,0 +1,22 @@
+import org.sql2o.*;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.util.List;
+
+public class Sightings {
+    private String location;
+    private String rangerName;
+    private Timestamp spotted;
+    private int animalId;
+    private int id;
+
+    public Sightings(String location, String rangerName, int animalId) {
+        this.location = location;
+        this.rangerName = rangerName;
+        this.animalId = animalId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
