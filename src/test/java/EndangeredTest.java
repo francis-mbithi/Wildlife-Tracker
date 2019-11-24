e5bf27189e554a1428bcfec03bfa3091806e6fe6
francis-mbithi <frankmbithifm@gmail.com>
2019-11-24 13:27:06 +0300

    test endangered animal health

--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -1,19 +1,27 @@
-package wildlifeTracker;
+//package wildlifeTracker;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
 import java.util.List;
 
-public class EndangeredTest{
-//    @Rule
-////    public DatabaseRules database = new DatabaseRules();
+public class EndangeredTest {
+    @Rule
+    public DatabaseRules database = new DatabaseRules();
+
     @Test
-    public void endangered_instantiatesCorrectly_true(){
-        Endangered testAnimal = new Endangered("lion","okay", "young");
+    public void endangered_instantiatesCorrectly_true() {
+        Endangered testAnimal = new Endangered("lion", "okay", "young");
         assertEquals(testAnimal instanceof Endangered, true);
     }
+
     @Test
-    public void endangered_instantiatesWithAnimalName_String(){
-        Endangered testAnimal = new Endangered("lion","okay", "young");
+    public void endangered_instantiatesWithAnimalName_String() {
+        Endangered testAnimal = new Endangered("lion", "okay", "young");
         assertEquals("lion", testAnimal.getName());
     }
+    @Test
+    public void endangered_instantiatesWIthAnimalHealth_String(){
+        Endangered testAnimal = new Endangered("lion","okay", "young");
+        assertEquals("okay", testAnimal.getHealth());
+    }
+}
